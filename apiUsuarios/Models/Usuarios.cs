7247fde6e19affbbf6514d3c9760a8048c0dc927
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2024-02-04 11:43:56 -0500

    first commit

---
+++ apiUsuarios/Models/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace apiUsuarios.Models
+{
+    [Table("usuarios")]
+    public class Usuarios
+    {
+        [Key]
+        public int id { get; set; }
+        public string apellidos { get; set; }
+        public string nombres { get; set; }
+        public string correo { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+    }
+}
